e86a24c5ae8a61738efe696fb3645b175d56651a
Ben Cochran <bcochran@gmail.com>
2009-10-20 22:44:19 -0500

    ivars for GNToggleItem

---
+++ GNToggleItem.h
@@ -0,0 +1,20 @@
+//
+//  GNToggleItem.h
+//  GNToggleBar
+//
+//  Created by Ben Cochran on 10/20/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GNToggleItem : NSObject {
+	NSString *_title;
+	NSString #_icon;
+}
+
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *icon;
+
+@end
---
+++ GNToggleItem.m
@@ -0,0 +1,16 @@
+//
+//  GNToggleItem.m
+//  GNToggleBar
+//
+//  Created by Ben Cochran on 10/20/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "GNToggleItem.h"
+
+
+@implementation GNToggleItem
+
+@synthesize title=_title, icon=_icon;
+
+@end
